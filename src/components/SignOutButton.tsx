cebeb9e305fee46de5e590620631e2619c9dfe38
IH <iwasaki.tandn8810@icloud.com>
2024-05-15 15:09:44 +0900

    UIちょっとだけ

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -2,17 +2,18 @@
 import React from 'react';
 
 import { signOut, useSession } from 'next-auth/react';
+import { Button } from './ui/button';
 
 const SignOutButton = () => {
   return (
-    <button
+    <Button
       type='button'
       onClick={async () => {
         await signOut();
       }}
     >
       SignOut
-    </button>
+    </Button>
   );
 };
 
